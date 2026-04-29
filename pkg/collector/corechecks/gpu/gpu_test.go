b131f435e7a93d59a5573db28ab9c43754f48135
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-04-29 15:28:49 +0200

    Fix test

--- pkg/collector/corechecks/gpu/gpu_test.go
+++ pkg/collector/corechecks/gpu/gpu_test.go
@@ -235,7 +235,7 @@ func TestRunDoesNotError(t *testing.T) {
 
 func TestCollectorsOnDeviceChanges(t *testing.T) {
 	// note: bump this when we'll add new collectors in nvidia.BuildCollectors
-	const numSupportedCollectorTypes = 6
+	const numSupportedCollectorTypes = 7
 
 	// mock up device count so that we can check when check collectors are created/destroyed
 	nvmlMock := testutil.GetBasicNvmlMockWithOptions(
@@ -300,7 +300,7 @@ func TestCollectorsOnDeviceChanges(t *testing.T) {
 
 func TestCollectorsOnMIGDeviceChanges(t *testing.T) {
 	// note: bump this when we'll add new collectors in nvidia.BuildCollectors
-	const numSupportedCollectorTypes = 5
+	const numSupportedCollectorTypes = 7
 
 	// Use device index 5 which has MIG support in testutil
 	deviceIdx := 5
